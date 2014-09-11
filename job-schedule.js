a16fe1df589cdfaea7966c4d44f410723b41ef0e
dreamweaver1231 <dreamweaver1231@gmail.com>
2014-09-11 11:43:01 +0000

    working scheduler: dont forget to clear db

--- job-schedule.js
+++ job-schedule.js
@@ -1,14 +1,12 @@
 exports.setupJobs = function() {
-	
-    var sampleJob = require('./jobs/sample-job.js');
-    // Setup agenda
-    var Agenda = require('agenda');
-    var agenda = new Agenda({
-        db: {
-            address: 'localhost:27017/myApp'
-        }
-    });
-    sampleJob.showMessage(agenda);
-    agenda.every('60 seconds', 'show message');
-    agenda.start();
+	var sampleJob = require('./jobs/sample-job.js');
+
+	// Setup agenda
+	var Agenda = require('agenda');
+	var agenda = new Agenda({db: { address: 'localhost:27017/myApp'}});
+
+	sampleJob.showMessage(agenda);
+	agenda.every('2 seconds', 'show message');
+
+	agenda.start();
 }
